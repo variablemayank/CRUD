01e6af10bb8e5b3ac9851ef21845d9bb9782fa80
variablemayank <mayank202020@rediffmail.com>
2018-07-17 23:10:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+const products = [
+  {
+    name: 'Ipad',
+    price: '200'
+  },
+  {
+    name: 'iPhone',
+    price: '650'
+  }
+];
+
+localStorage.setItem('products',JSON.stringify(products));
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products:[]
+    };
+  }
+
+  ComponentWillMount() {
+  this.getProducts();
+  }
+  getProducts() {
+    const products = JSON.parse(localStorage.getItem('products'));
+    this.setState({products});
+  }
+  render() {
+    return (
+      <div className="App">
+      <h1>Products Manager </h1>
+      {
+        this.state.products.map(product => {
+          return (
+            <div key = {product.name}>
+            <span>{product.name}</span>
+            {` | `}
+            <span>{product.price}</span>
+            {` | `}
+            <button>Delete</button>
+            </div>
+          );
+        })
+      }
+      </div>
+    );
+}
+}
+
+
+export default App;
